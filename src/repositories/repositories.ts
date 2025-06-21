61544feb61fae8284b6e634a7a2007059f72d2b7
amitVaibhavTiwari <amitvaibhavtiwari@gmail.com>
2025-06-22 01:55:34 +0530

    removed some unwanted entities and resolvers

--- src/repositories/repositories.ts
+++ src/repositories/repositories.ts
@@ -1,8 +1,4 @@
 import { AppDataSource } from "../dataSource/dataSource.js";
-import { Books } from "../entities/Books.js";
-import { Librarians } from "../entities/librarians.js";
-import { Loans } from "../entities/Loans.js";
-import { Students } from "../entities/students.js";
 import { AccountUser } from "../entities/AccountUser.js";
 import { UserAddress } from "../entities/UserAddress.js";
 import { UserEmail } from "../entities/UserEmail.js";
@@ -20,11 +16,6 @@ import { WarehouseStock } from "../entities/WarehouseStock.js";
 import { ProductType } from "../entities/ProductType.js";
 import { ShopMeta } from "../entities/ShopMeta.js";
 
-export const bookRepository = AppDataSource.getRepository(Books);
-export const librarianRepository = AppDataSource.getRepository(Librarians);
-export const studentRepository = AppDataSource.getRepository(Students);
-export const loanRepository = AppDataSource.getRepository(Loans);
-// delete above repositories later
 export const accountUserRepository = AppDataSource.getRepository(AccountUser);
 export const userEmailRepository = AppDataSource.getRepository(UserEmail);
 export const userPhoneRepository = AppDataSource.getRepository(UserPhone);
